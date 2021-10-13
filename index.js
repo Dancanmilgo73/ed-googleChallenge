94fd74aec6700905cb96cc2944afb618154d8a67
Dan Ngetich <dancanmilgo73@gmail.com>
2021-10-13 16:55:53 +0300

    Worked on the design of the page and populate the cards with data via javascript

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// splice(start, deleteCount, item)
 // slice(start, end)
 //  returns a shallow copy of a portion of an array into a new array object selected from start to end
 
@@ -5,7 +6,6 @@
 // acc: the value resulting from the previous call to callbackfn
 // curr: value of the current elem;
 // example: let sum = [1,2,3].reduce((acc, curr)=>acc+=curr);
-// splice(start, deleteCount, item)
 // start: remove elements starting from this index
 // deleteCount: A number indicating the number of elements to remve beginning from start
 // item: The element(s) to add to the array beginning from start
