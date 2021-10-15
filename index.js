96781e9a1b60d6294fff5412d1315cbf8464a5a2
Dan Ngetich <dancanmilgo73@gmail.com>
2021-10-15 09:51:40 +0300

    space optimization

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-// splice(start, deleteCount, item)
-// slice(start, end)
-//  returns a shallow copy of a portion of an array into a new array object selected from start to end
+const arrayManipulation = (arr) => {
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] === "+") arr.splice(i, 1, arr[i - 1] + arr[i - 2]);
+    if (arr[i] === "D") arr.splice(i, 1, arr[i - 1] * arr[i - 2]);
+    if (arr[i] === "C") {
+      arr.splice(i - 1, 2);
+      i = i - 2;
+    }
 
-// reduce((acc, curr)=> acc+curr);
-// acc: the value resulting from the previous call to callbackfn
-// curr: value of the current elem;
-// example: let sum = [1,2,3].reduce((acc, curr)=>acc+=curr);
-// start: remove elements starting from this index
-// deleteCount: A number indicating the number of elements to remve beginning from start
-// item: The element(s) to add to the array beginning from start
-function arrayManipulation(arr) {
-  const result = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === "number") {
-      result.push(arr[i]);
-    }
-    if (i > 1) {
-      if (arr[i] === "+") {
-        result.push(result[result.length - 1] + result[result.length - 2]);
-      }
-      if (arr[i] === "D") {
-        result.push(result[result.length - 1] * result[result.length - 2]);
-      }
-      if (arr[i] === "C") {
-        result.pop();
-      }
-    }
+    arr[i];
   }
-  console.log(result.reduce((acc, curr) => (acc += curr)));
-}
+  return arr;
+};
 
-arrayManipulation([10, 9, "+", "D", "C", "+"]);
+console.log(arrayManipulation([10, 9, 19, 171, "C", "+"]));
